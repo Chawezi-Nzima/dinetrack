723ea276f7069a151b5638d9259a7755982848f4
969 <tanilrego@gmail.com>
2025-12-12 11:50:56 +0200

    login4

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e9c4c1ddedcb96100267c8617397a4de
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "d51365e2341b0e188b4e7be92340bfe0",
 "/": "d51365e2341b0e188b4e7be92340bfe0",
-"main.dart.js": "550bee8b8a009fa20d2b2b284071d374",
+"main.dart.js": "85b9852a09f0d60e1681b6ead2d0c83a",
 "manifest.json": "50344acacb0bcf53b319bbc5fac2290b",
 "version.json": "b22efff51d82d1b6c07dce8f3563be33"};
 // The application shell files that are downloaded before a service worker can
